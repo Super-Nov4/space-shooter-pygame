d5206933908344f5b379ebea47de85f09b165cdf
Super-Nov4 <i.qwerty.xd.kp@gmail.com>
2024-10-09 01:01:33 +0300

    player add components

--- entities/player.py
+++ entities/player.py
@@ -12,3 +12,5 @@ class PlayerShip(Entity):
         self.add_component("Velocity", VelocityComponent(0, 0))
         self.add_component("Position", PositionComponent(0, 0))
         self.add_component("Sprite", SpriteComponent(asset_manager.get_cached("player_ship")))
+
+        """Здесь должна быть реализована стрельба, движение игрока?"""
